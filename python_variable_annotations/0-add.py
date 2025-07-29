2c0468eda1b5809dd7d4fdea73217d54296abdbb
Bryanjose001 <bryan.jose2541@gmail.com>
2025-07-29 12:13:29 -0400

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-This script defines a function 
+This script defines a function
 """
 
-def add(a: float, b: float) -> float:
+
+def add(a:float, b:float) -> float:
     """ Add two floating-point numbers."""
     return a + b
