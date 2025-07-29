d92f76e62e7c5ba8a77723f3cf1faca6a5960a97
Bryanjose001 <bryan.jose2541@gmail.com>
2025-07-29 12:10:41 -0400

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """
-This script defines a function to add two floating-point numbers
-and uses type annotations to specify the expected types of the parameters and return value.
+This script defines a function
     
 """
-
 
 def add(a: float, b: float) -> float:
     """ Add two floating-point numbers."""
