9b36c4f1e2ae2bc4d8802782a2ccd8074bbc148f
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-19 15:47:32 -0400

    lint therm all

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,14 +1,37 @@
-// eslint.config.js
-export default [
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'off',
-    },
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
-];
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
