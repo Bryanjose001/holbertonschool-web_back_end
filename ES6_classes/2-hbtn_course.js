8ba4fff298fdb33f113c0f365eb06eb701df5ee2
bryanjosepr <bryan.jose2541@gmail.com>
2025-08-22 12:36:46 -0400

    task fixing

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,57 +1,48 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // name
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
     if (typeof name === 'string') {
       this._name = name;
     } else {
       throw new TypeError('Name must be a string');
     }
+  }
 
+  // length
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
     if (typeof length === 'number') {
       this._length = length;
     } else {
       throw new TypeError('Length must be a number');
     }
-
-    if (Array.isArray(students) && students.every((s) => typeof s === 'string')) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array of strings');
-    }
   }
 
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    if (typeof value === 'string') {
-      this._name = value;
-    } else {
-      throw new TypeError('Value must be a string');
-    }
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  set length(value) {
-    if (typeof value === 'number') {
-      this._length = value;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
-  }
-
+  // students
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    if (Array.isArray(value) && value.every((student) => typeof student === 'string')) {
-      this._students = value;
+  set students(students) {
+    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
+      this._students = students;
     } else {
-      throw new TypeError('Students must be an array of strings');
+      throw new TypeError('Students must be an array');
     }
   }
 }
+
+export default HolbertonCourse;
