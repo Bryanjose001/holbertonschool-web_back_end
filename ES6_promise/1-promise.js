94f907995648547c4c2b193a054190a4eada6a85
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-27 15:40:57 -0400

    REject eagain not traying anymor3e

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -3,6 +3,6 @@
         if (true){
             resolve({status: 200, body: 'Success'});
         } else {
-            reject(new Error('The fake API is not working currently'));}
+            reject(Error('The fake API is not working currently'));}
     });
 }
