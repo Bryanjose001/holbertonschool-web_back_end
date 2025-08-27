24b2ed4cacab291993993042b3de9b82f84c0604
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-27 15:35:30 -0400

    if succses return

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
  export default function getFullResponseFromAPI(){
     return new Promise((resolve, reject) =>{
-        if (true){
+        if (success){
             resolve({status: 200, body: 'Success'});
         } else {
-            reject(Error('The fake API is not working currently'));
+            reject(Error("The fake API is not working currently"));
         }
     });
 }
