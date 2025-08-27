ad1a2535e2fbba7936799ab47ab223926e63160c
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-27 15:32:25 -0400

    just trrying to make it

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -3,7 +3,7 @@
         if (true){
             resolve({status: 200, body: 'Success'});
         } else {
-            reject(new Error('The fake API is not working currently'));
+            reject(Error('The fake API is not working currently'));
         }
     });
 }
