377a00b7f00733cf3230a51c83b49c297d4da16b
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-28 16:00:56 -0400

    fix: correct condition in promise resolution

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
  export default function getFullResponseFromAPI(Success){
     return new Promise((resolve, reject) =>{
-        if (true){
+        if (Success){
             resolve({status: 200, body: 'Success'});
         } else{
             reject(Error('The fake API is not working currently'));
