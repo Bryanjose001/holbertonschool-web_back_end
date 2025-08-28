95622941856698a619e5ed2a30cb2cbb989fb778
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-28 16:24:44 -0400

    returning the promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,7 @@
 function handleResponseFromAPI(promise)
 {
-    return promise
-        .then((result) => {
-            console.log('Got a response from the API');
-            return result;
-        })
-        .catch((error) => {
-            console.log(error.message);
-        });
+    return promise(
+        
+    )
+        
 }
