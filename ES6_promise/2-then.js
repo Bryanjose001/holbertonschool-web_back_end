9da626b2884f87cb2578167a96a758d81fc74fa2
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-28 16:12:40 -0400

    fix: add promise handling in handleResponseFromAPI function

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,3 +1,11 @@
 function handleResponseFromAPI(promise)
 {
-    
+    return promise
+        .then((result) => {
+            console.log('Got a response from the API');
+            return result;
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+}
