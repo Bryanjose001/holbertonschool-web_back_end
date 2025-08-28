7fe9fe7959cc0917acd35f303c9274ba6209fcbe
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-28 17:18:51 -0400

    just using t the 3 methods for the console log

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,22 @@
-function handleResponseFromAPI(promise)
-{
-    return promise(
-        
-    )
-        
+function handleResponseFromAPI(promise) {
+    const promise = getFullResponseFromAPI(true);
+        let success = true;
+        if (success){
+            promise.resolve().then(result => console.log(result));
+        } else{
+            promise.reject().then(result => console.log(result));
+        }
 }
+
+//function handleResponseFromAPI(promise) {
+    //return promise
+    //.then(() => {
+        //return { status: 200, body: "success"};
+    //})
+    //.catch(() => {
+        //return new Error();
+    //})
+    //.finally(() => {
+        //console.log("Got a response from the API");
+    //});
+//}
