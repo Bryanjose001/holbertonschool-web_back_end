3599a5b5de10c6f30b8cf5e16548c51c32d215e6
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-30 15:59:44 -0400

    Fix syntax error in handleProfileSignup function

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signupuser from '4-user-promise.js';
+import uploadphoto from '5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName)
+{
+    return signupuser(firstName, lastName)
+    
+    });
+}
