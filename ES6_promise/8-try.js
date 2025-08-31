bcbfd6732767c0e60a75be65ec8763ae0d153cf0
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-31 16:21:13 -0400

    refactor divideFunction to remove promise and handle division directly

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,9 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-    return new Promise((resolve, reject) => {
         if (denominator === 0) {
-            reject(new Error('Cannot divide by 0'));
-        } else {
-            resolve(numerator / denominator);
-        }
-    });
-}
+           throw new Error('Cannot divide by 0');
+        }  
+            return numerator / denominator;
+    }
