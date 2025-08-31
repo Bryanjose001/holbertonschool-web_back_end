edd8a58771e9c8d98165c49327ec90a716acd361
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-31 16:02:33 -0400

    correct smplify promise handling

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
-import signupuser from '4-user-promise.js';
-import uploadphoto from '5-photo-reject.js';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName)
-{
-    const thepromise1 = signupuser(firstName, lastName)
-    const thepromise2 = uploadphoto(fileName)
-    return Promise.allSettled([thepromise1, thepromise2
-    ]).then((results) => {
-        return results.map((result) => ({
-            status: result.status, value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
-        }));
-    })
-    };
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))
+  );
+}
