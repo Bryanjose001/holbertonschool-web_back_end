c8b3d0cddf3f8ac8e9221903a1b92140c81bf07b
Bryanjose001 <bryan.jose2541@gmail.com>
2025-08-31 16:10:51 -0400

    i thnk this is the fastes return

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,5 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-    return Promise.all([chinaDownload, USDownload])
-        .then((results) => (results[0] < results[1] ? 'China' : 'US'));
-
+    return Promise
 }
