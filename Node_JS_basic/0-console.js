358a4a4ff2c74ff8285fc63fe22da4cd3751ffe1
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-01 15:16:26 -0400

    back to the begining learning more about node js

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,19 +1,4 @@
-//const displayMessage =
-//function displayMessage() {
-    //console.log("Hello NodeJS!");
-//}
-
-const { createServer } = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Hello NodeJS!");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const displayMessage =
+function displayMessage() {
+  console.log("Hello NodeJS!");
+}
