62301c14e342f71a20d44f058071bed93f3150f0
Bryan Camacho <bryanjose431@gmail.com>
2025-12-01 13:07:03 -0400

    task 1 learning pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""function that returns a list of pagination parameters"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """function that has two interger arguments and returns a parameter"""
+    return (((page * page_size) - page_size), (page * page_size))
