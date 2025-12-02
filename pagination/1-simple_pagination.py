6b440d03579811cd92f0d0763a243200d52a12fe
Bryan Camacho <bryanjose431@gmail.com>
2025-12-02 15:12:13 -0400

    new line at the end  of the code

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -30,6 +30,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            """functions that uses assert for page maniging"""
             assert type(page) is int and page > 0 
             assert type(page_size) is int and page_size > 0
             data = Server.dataset(self)
