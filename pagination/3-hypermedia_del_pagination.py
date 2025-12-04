2b4e4da1e1c232ee0f9d8d236c16491c8d87976e
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-04 15:02:18 -0400

    3-blank

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List,Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names. """
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
