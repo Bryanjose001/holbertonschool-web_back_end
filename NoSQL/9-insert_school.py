0ab0c12f15a2aa0d033707aaae49c5846b93d82c
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-07 16:18:31 -0500

    inserts doc into a collection

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Inserts doc into a collection"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts doc into a collection"""
+
+    my_documentation = mongo_collection.insert_one(kwargs)
+    return my_documentation.
