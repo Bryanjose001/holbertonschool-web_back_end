39c1ad4416a652ec5b15c49fb3e404d98e1c7334
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-13 15:49:35 -0400

    fixing the syntax for es6 and making the loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+ let array = [];
+    for (let value in array) {
+    array.push(appendString + value);
   }
 
   return array;
