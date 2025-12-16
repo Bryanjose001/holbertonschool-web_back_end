262c52db1314ec5d236db355c1808b78c7e1ea8b
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-16 15:55:56 -0400

    all the files that i =had plus the task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents(){
+    let obj = {id:'1',firstName:'Guillaume',location: 'San Francisco'}
+    let obj1 = {id:'2',firstName:'James',location: 'Columbia'}
+    let obj2 = {id:'5',firstName:'Serena',location: 'San Francisco'}
+    let myarray = [obj,obj1,obj2]
+    return myarray
+}
