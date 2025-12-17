988c8ab252b5613897589f4ad9d7a2c4deb3cf46
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-17 13:03:25 -0400

    fixing the condition on the task1 so if the array is => empty

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
-export default function getListStudentIds(){
-    let obj = {id:1}
-    let obj1 = {id:2}
-    let obj2 = {id:5}
-    let studentIds = [obj,obj1,obj2];
-    let myMap = new Map(studentIds);
-    return myMap
+export default function getListStudentIds(id_arrays){
+    if(!Array.isArray(id_arrays)){
+        return[];
+    }
+
+    return 
 }
