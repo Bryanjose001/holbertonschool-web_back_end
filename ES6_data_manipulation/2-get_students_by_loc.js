9f9e9c6cfaddb8de40695cbeb4f1915881c3b98d
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-17 13:33:48 -0400

    i think i retun the list of the students

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,7 @@
+import getListStudents from "./0-get_list_students"
+
 export default function getStudentsByLocation(city,myStudents){
     let myarray = myStudents
-    
-
+    myStudents = getListStudents();
     return myStudents
 }
