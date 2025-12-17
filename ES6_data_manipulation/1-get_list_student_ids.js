5844f3336234b89ae3d2d21052159f0b13787c6d
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-17 13:16:18 -0400

    fixing the map function

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,5 +3,5 @@ export default function getListStudentIds(id_arrays){
         return[];
     }
 
-    return id_arrays.map((id_arrays) => array)
+    return id_arrays.map((id_arrays) => id_arrays)
 }
