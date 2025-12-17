ffa003f226f14f680cde1fa78cdd47e303fc3456
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-17 12:44:08 -0400

    fixing thew syntax

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,8 @@
-export default function getListStudentIds(studentIds){
-    let studentIds = new Map 
+export default function getListStudentIds(){
+    let studentIds = new Map();
     studentIds.set(id,1);
-    studentIds.set(id,1);
-    studentIds.set(id,1);
+    studentIds.set(id,2);
+    studentIds.set(id,5);
     let myarray = [studentIds]
     return my_array
 }
