c7681a8a17dd7574a53b5182097a1b115004abea
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-17 15:47:37 -0400

    task 4 updating the grades

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(myStudents,city,newGrades){
+    let obj = {
+        studentId: 31,
+        grade: 78,
+    }
+    if(myStudents != newGrades)
+        
+    return 
+}
