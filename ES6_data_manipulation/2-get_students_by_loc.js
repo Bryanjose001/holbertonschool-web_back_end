cef695610bf3711747cb594d8532927f46b96424
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-17 13:40:55 -0400

    fixing the method that i use since the one i was doing doesnt work

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,6 @@
-import getListStudents from "./0-get_list_students"
-
 export default function getStudentsByLocation(city,myStudents){
-    let myarray = myStudents
-    myStudents = getListStudents();
-    return myStudents
+    if(!Array.isArray(myStudents)){
+        return[];
+    }
+    return myStudents.filter((myStudents) => myStudents.location === city)
 }
