c115cf67508806b11a732cdfa402c8aaac85f769
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-18 13:30:07 -0400

    first try to get the typed arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,5 @@
-export default function createInt8TypedArray(){
-    return 
+export default function createInt8TypedArray(lenght,position,value){
+    let buffer = new ArrayBuffer(8);
+    let view = new Int32Array(buffer);
+    return ArrayBuffer
 }
