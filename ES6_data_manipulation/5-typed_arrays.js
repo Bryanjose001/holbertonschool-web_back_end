6d70b9a3ac695ca9d9b4681c8274a9a716fac259
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-18 13:41:44 -0400

    typed arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(lenght,position,value){
     let buffer = new ArrayBuffer(8);
-    let view = new Int32Array(buffer);
-    return ArrayBuffer
+    let view = new DataView(buffer);
+    lenght = 10
+    position = 2
+    value = 89
+    DataView.setInt8(position) = value
+    return 
 }
