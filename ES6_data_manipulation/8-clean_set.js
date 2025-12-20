0bdcdc4e367da76671b6ae0dd90c9df776eea7c5
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-20 14:26:44 -0400

    task 8 tested and run correct

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,15 @@
- 
+ export default function cleanSet(set, startString) {
+  let result = [];
+
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  for (let value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}
