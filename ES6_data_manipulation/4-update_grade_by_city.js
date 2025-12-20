8c4b91199746ceff62b3fc8f278b8c308dc4ca70
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-20 14:14:42 -0400

    i got it

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(myStudents,city,newGrades){    
     return myStudents.filter((student) => student.location === city).map((student) =>{
-        let mystudentstsgrades = newGrades.find((grade) => grade.id === student.id)
-    return {...student,mystudentstsgrades:97}
+        let mystudentstsgrades = newGrades.find((grade) => grade.studentId === student.id)
+    return {...student,grade: mystudentstsgrades ? mystudentstsgrades.grade : 'N/A'}
     } )
 }
