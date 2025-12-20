d171112dbf84fafdc477d50fb5fcc324cc4e3c9b
Bryanjose001 <bryan.jose2541@gmail.com>
2025-12-20 13:53:46 -0400

    i dont know i think im closer to making the task but i need to make the condition

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(myStudents,city,newGrades){    
     return myStudents.filter((student) => student.location === city).map((student) =>{
-        let mystudnetsgrades = newGrades.find((myvar) => myvar.id === student.id)
-    return {...student,mystudnetsgrades}
+        let mystudentstsgrades = newGrades.find((grade) => grade.id === student.id)
+    return {...student,mystudentstsgrades:97}
     } )
 }
