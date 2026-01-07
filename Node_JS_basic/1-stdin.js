b580aafbc10ae691ee2a6a89d08039585385c9fa
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-07 10:37:33 -0400

    task 2 procces of the inp and out

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+
+process.stdin.on('data' , (data) => {
+    let name = data.toString().trim();
+    console.log('Your name is, ${name}');
+});
+
+process.stdin.on('end' , () => {
+    console.log('This important software is now closing');
+});
