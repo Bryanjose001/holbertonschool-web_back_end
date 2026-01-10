7eb4710871066be174b5eb4775b77e8d786d65dc
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-10 15:31:46 -0400

    task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,16 +1,31 @@
 let fs = require('fs');
 
-function countStudents(){
-    let databasecsv1 = './database.csv'
+function countStudents() {
+  let databasecsv1 = './database.csv';
 
-    try{
-        let database = fs.readFileSync(databasecsv1, 'utf-8')
-        let lines = database.trim().split('\n')
-        let numerofStudents = lines.length - 1
-        console.log(`Number of students: ${numerofStudents}`)
-    } catch(error1){
-    throw new Error('Cannot load the database');
+  try {
+    let database = fs.readFileSync(databasecsv1, 'utf-8');
+    let lines = database.trim().split('\n');
+    let numberOfStudents = lines.length - 1;
+    console.log(`Number of students: ${numberOfStudents}`);
+    let students = lines.slice(1);
+    let fields = {};
+    students.forEach((line) => {
+      let parts = line.split(',');
+      let firstName = parts[0];
+      let field = parts[parts.length - 1];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    });
+    for (let field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
     }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
