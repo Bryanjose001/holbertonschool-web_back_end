b1825983ff882323d72e60ea8446f8b9e9ab3838
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-12 14:20:48 -0400

    fixing a more complex HTTP server using Express

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -3,35 +3,25 @@ const fs = require('node:fs/promises');
 async function countStudents(path) {
   try {
     const database = await fs.readFile(path, 'utf-8');
-
-    const lines = database.split('\n');
+    const lines = database.trim().split('\n');
     lines.shift();
 
-    const students = lines.filter((line) => line.trim() !== '');
-
-    let output = `Number of students: ${students.length}\n`;
+    const students = lines.filter((line) => line);
+    let output = `Number of students: ${students.length}`;
 
     const fields = {};
 
     students.forEach((line) => {
-      const parts = line.split(',');
-      const firstName = parts[0];
-      const field = parts[parts.length - 1];
-
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-
-      fields[field].push(firstName);
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstname);
     });
 
     for (const field in fields) {
-      if (Object.prototype.hasOwnProperty.call(fields, field)) {
-        output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-      }
+      output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
     }
 
-    return output.trim();
+    return output;
   } catch (error) {
     throw new Error('Cannot load the database');
   }
--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = express();
+const DATABASE = process.argv[2];
 
 app.get('/', (request, response) => {
   response.status(200).send('Hello Holberton School!');
@@ -13,10 +15,10 @@ app.get('/students', async (request, response) => {
   response.write('This is the list of our students\n');
 
   try {
-    await countStudents(DATABASE);
-    response.end();
+    const dataResult =await countStudents(DATABASE);
+    response.status(200).send(dataResult)
   } catch (error) {
-    response.end(error.message);
+    response.status(200).send(error.message);
   }
 });
 
