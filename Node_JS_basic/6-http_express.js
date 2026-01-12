ee96f8fd462aedd3685c32a0b91f785b357e3e93
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-12 13:55:30 -0400

    fixing the express app server

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,13 +1,12 @@
-const http = require('http');
 const express = require('express');
 
 const hostname = '127.0.0.1';
 const port = 1245;
+
 const app = express();
 
-app.get((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello Holberton School!');
+app.get('/', (request, response) => {
+  response.status(200).send('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
