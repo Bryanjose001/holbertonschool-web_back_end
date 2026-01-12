44b7fdd087b1e3a03b0c54ede47ac5ac7611006e
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-12 13:34:04 -0400

    fixing the lint in the task

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,20 +3,19 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 const countStudents = require('./3-read_file_async');
+
 const DATABASE = process.argv[2];
 
 const app = http.createServer(async (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  
 
   const { url } = request;
-  if(url ==='/') {
+  if (url === '/') {
     response.end('Hello Holberton School!');
-
-  }else if(url === '/students'){
+  } else if (url === '/students') {
     response.write('This is the list of our students\n');
 
-    try{
+    try {
       const students = await countStudents(DATABASE);
       response.end(students);
     } catch (error) {
