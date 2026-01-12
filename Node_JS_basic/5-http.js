21d53f1c0b0e9a9d5e424e7743f060e5278ac3da
Bryanjose001 <bryan.jose2541@gmail.com>
2026-01-12 13:15:22 -0400

    a more complex HTTP server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,10 +2,27 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
+const countStudents = require('./3-read_file_async');
+const DATABASE = args[0];
 
-const app = http.createServer((request, response) => {
+const app = http.createServer(async (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello Holberton School!');
+  
+
+  const { url } = request;
+  if(url ==='/') {
+    response.write('Hello Holberton School!');
+  }else if(url === '/students'){
+    response.write('This is the list of our students\n');
+    try{
+      const students = await countStudents(DATABASE);
+      response.end(`${students.join('\n')}`);
+    } catch (error) {
+      response.end(error.message);
+    }
+  }
+  response.statusCode = 404;
+  response.end();
 });
 
 app.listen(port, hostname, () => {
